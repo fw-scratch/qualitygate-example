e9edf28d35316286112b72390ddb47f12808322e
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-05-27 09:43:25 +0200

    first commit

---
+++ src/main/java/com/example/qualitygateexample/QualitygateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.qualitygateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitygateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualitygateExampleApplication.class, args);
+	}
+
+}
